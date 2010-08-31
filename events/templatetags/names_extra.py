b79eb5018657779ff61044f930d4d96540e8d0bf
Rodrigo Setti <rodrigosetti@gmail.com>
2010-08-31 16:18:56 -0300

    GMT+3, gravatar_url tag

--- events/templatetags/names_extra.py
+++ events/templatetags/names_extra.py
@@ -1,14 +1,26 @@
 from django.template.defaultfilters import stringfilter
 from django import template
-from string import lower, capitalize
+import urllib, hashlib, sys
 
 register = template.Library()
 
 @register.filter
 @stringfilter
 def capsname(value):
-	"Capitalizes full name"
-	return ' '.join([ (capitalize(name) if 
-					  (name not in ("de", "da", "e")) else name)
-					  for name in lower(value).split() ])
+    "Capitalizes full name"
+
+    return ' '.join([ (name.capitalize() if 
+                      (name not in ("de", "da", "e")) else name)
+                      for name in value.lower().split() ])
+
+@register.filter
+@stringfilter
+def gravatar_url(value, default="404", size=32):
+    "Gets gravatar URL"
+   
+    # returns generated URL
+    return "http://www.gravatar.com/avatar/%s?%s" % (
+         hashlib.md5(value.lower()).hexdigest(),
+         urllib.urlencode({'d':default, 's':str(size)})
+        )
 
